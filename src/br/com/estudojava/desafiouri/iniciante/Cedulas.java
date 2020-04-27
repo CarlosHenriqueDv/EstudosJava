5c432cb149a8d1945def9c850b716cad2cb7a0ea
CarlosHenriqueDv <cshenrique02@gmail.com>
2020-04-27 08:23:07 -0300

    Commit classe cedulas

--- src/br/com/estudojava/desafiouri/iniciante/Cedulas.java
+++ src/br/com/estudojava/desafiouri/iniciante/Cedulas.java
@@ -1,4 +1,21 @@
 package br.com.estudojava.desafiouri.iniciante;
 
+import java.util.Scanner;
+
 public class Cedulas {
+
+    public static void main(String[] args) {
+
+        int valorDeEntrada;
+
+        //Notas
+
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Informe um valor");
+        valorDeEntrada = s.nextInt();
+
+
+    }
 }
