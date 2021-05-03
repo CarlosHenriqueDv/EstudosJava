ebe6be2ad3cdc977c065ca96f9b547407ec12fa7
Carlos Henrique <cshenrique02@gmail.com>
2021-05-02 21:51:24 -0300

    desafio uri nivel facil

--- src/br/com/estudojava/desafiouri/iniciante/Main.java
+++ src/br/com/estudojava/desafiouri/iniciante/Main.java
@@ -2,35 +2,47 @@ package br.com.estudojava.desafiouri.iniciante;
 
 import java.util.Scanner;
 
-public class Cedulas {
+public class Main {
+
+    private enum notas{
+        CEM(100), CINQUENTA(50), VINTE(20), DEZ(10), CINCO(5),
+        DOIS(2), UM(1);
+        private int valor;
+        notas(int valor) {
+        }
+    }
+
+
 
     public static void main(String[] args) {
 
         int valorDeEntrada;
 
         //Notas
+        System.out.println("Informe o valor: ");
         Scanner entrada = new Scanner(System.in);
+
         int notas100, notas50, notas20, notas10,
                 notas5, notas2, notas1;
 
-        int valorentrada = entrada.nextInt();
+        int valorEntrada = entrada.nextInt();
 
-        float valor = valorentrada;
-        notas100 = (int) (valor / 100);
+        float valor = valorEntrada;
+        notas100 = (int) (valor / notas.CEM.valor);
         valor %= 100;
-        notas50 = (int) (valor / 50);
+        notas50 = (int) (valor / notas.CINQUENTA.valor);
         valor %= 50;
-        notas20 = (int) (valor / 20);
+        notas20 = (int) (valor / notas.VINTE.valor);
         valor %= 20;
-        notas10 = (int) (valor / 10);
+        notas10 = (int) (valor / notas.DEZ.valor);
         valor %= 10;
-        notas5 = (int) (valor / 5);
+        notas5 = (int) (valor / notas.CINCO.valor);
         valor %= 5;
-        notas2 = (int) (valor / 2);
+        notas2 = (int) (valor / notas.DOIS.valor);
         valor %= 2;
-        notas1 = (int) (valor / 1);
+        notas1 = (int) (valor / notas.UM.valor);
         valor %= 1;
-        System.out.println(valorentrada);
+        System.out.println(valorEntrada);
         System.out.printf("%d nota(s) de R$ 100,00\n", notas100);
         System.out.printf("%d nota(s) de R$ 50,00\n", notas50);
         System.out.printf("%d nota(s) de R$ 20,00\n", notas20);
