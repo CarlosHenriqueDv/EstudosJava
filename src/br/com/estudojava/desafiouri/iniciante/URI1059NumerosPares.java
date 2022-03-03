249711ab8def6c14cbb3557747600544bb6f6814
carlos.mendonca <cshenrique02@gmail.com>
2022-03-02 23:04:30 -0300

    uri 1059 numeros pares

---
+++ src/br/com/estudojava/desafiouri/iniciante/URI1059NumerosPares.java
@@ -0,0 +1,14 @@
+package br.com.estudojava.desafiouri.iniciante;
+
+public class URI1059NumerosPares {
+
+    public static void main(String[] args){
+
+        for (int i = 1; i <=100; i++ ){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
